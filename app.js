d187c8c791cf2368371e04953e6f6f350725c152
Tomek28611 <tomek286@yahoo.com>
2022-10-10 14:06:03 +0200

    add port

--- app.js
+++ app.js
@@ -279,7 +279,7 @@ app.use((err, req, res, next) => {
     if(!err.message) err.message = 'Something went wrong!'
     res.status(statusCode).render('error', { err })
 })
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('port 3000');
 })
